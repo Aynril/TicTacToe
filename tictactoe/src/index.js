7a8824d18313780fd07a9a91f32c9e985dccd75f
Aynril <fabian.jaser@gmail.com>
2019-12-12 22:56:22 +0100

    Fixed a bug, works now

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -50,8 +50,6 @@ class Board extends React.Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
 
-    const status = 'Next player: '(this.state.xIsNext ? 'X' : 'O');
-
     return (
       <div>
         <div className="status">{status}</div>
